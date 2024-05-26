7df3419dc77e5bef03a3cf1d81aecf51ffaa2e4c
zhi_jun <liangzhijun1216@gmail.com>
2024-05-26 10:53:17 -0400

    added repsonive for title. and updated responsive for nav

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -8,37 +8,35 @@ import img2 from "./assets/img1.svg";
 const NavBar = () => {
   return (
     <>
-      <div className="">
-        <div className="bg-[#31468461] w-full h-[91px] p-4 md:px-10 shadow-nav fixed z-10">
-          <div className="flex flex-row justify-between items-center gap-[10px] px-5 md:gap-10 md:px-20 text-white">
-            <a href="#about_me" className="font-open-sans-hebrew text-[13px] md:text-[20px]">
-              About
-            </a>
-            <img src={img} alt="circle" className="hidden md:block"></img>
-            <img src={img2} alt="circle" className="block md:hidden"></img>
-            <a href="#skills" className="font-open-sans-hebrew text-[13px] md:text-[20px]">
-              Skills
-            </a>
-            <img src={img} alt="circle" className="hidden md:block"></img>
-            <img src={img2} alt="circle" className="block md:hidden"></img>
-            <a href="#title" className="font-brush-script-mt text-[42px] md:text-[48px]">
-              n2
-            </a>
-            <img src={img} alt="circle" className="hidden md:block"></img>
-            <img src={img2} alt="circle" className="block md:hidden"></img>
-            <a href="#project" className="font-open-sans-hebrew text-[13px] md:text-[20px]">
-              Projects
-            </a>
-            <img src={img} alt="circle" className="hidden md:block"></img>
-            <img src={img2} alt="circle" className="block md:hidden"></img>
-            <a href="#experience" className="font-open-sans-hebrew text-[13px] md:text-[20px]">
-              Experiences
-            </a>
-          </div>
+      <div className="bg-[#31468461] w-full h-[91px] p-4 md:px-10 shadow-nav fixed z-50 top-0 left-0">
+        <div className="flex flex-row justify-between items-center gap-[10px] px-5 md:gap-10 md:px-20 text-white">
+          <a href="#about_me" className="font-open-sans-hebrew text-[13px] md:text-[20px]">
+            About
+          </a>
+          <img src={img} alt="circle" className="hidden md:block"></img>
+          <img src={img2} alt="circle" className="block md:hidden"></img>
+          <a href="#skills" className="font-open-sans-hebrew text-[13px] md:text-[20px]">
+            Skills
+          </a>
+          <img src={img} alt="circle" className="hidden md:block"></img>
+          <img src={img2} alt="circle" className="block md:hidden"></img>
+          <a href="#title" className="font-brush-script-mt text-[42px] md:text-[48px]">
+            n2
+          </a>
+          <img src={img} alt="circle" className="hidden md:block"></img>
+          <img src={img2} alt="circle" className="block md:hidden"></img>
+          <a href="#project" className="font-open-sans-hebrew text-[13px] md:text-[20px]">
+            Projects
+          </a>
+          <img src={img} alt="circle" className="hidden md:block"></img>
+          <img src={img2} alt="circle" className="block md:hidden"></img>
+          <a href="#experience" className="font-open-sans-hebrew text-[13px] md:text-[20px]">
+            Experiences
+          </a>
         </div>
       </div>
 
-      <div className="flex flex-row gap-4 md:gap-10 justify-between w-[200px] md:w-[390px] mt-28 fixed px-5 md:px-10">
+      <div className="flex flex-row gap-4 md:gap-10 justify-between w-[200px] md:w-[390px] mt-28 fixed px-5 md:px-10 z-40">
         <a href="https://github.com/Liangzhijun123" target="_blank">
           <img src={github} alt="Github"></img>
         </a>
--- src/Title.tsx
+++ src/Title.tsx
@@ -1,19 +1,20 @@
 import img from "./assets/title.svg";
+import img1 from "./assets/img3.svg";
 
 const Title = () => {
     return (
       <>
-      <div id="title">
-      <img src={img} alt="img" className="w-full"></img>
-        <div className="flex flex-col justify-center items-center gap-5 font-arial mt-[-750px]">
-          <p className="text-white tracking-[7px] text-[32px]" >Web Development</p>
-          <p className="text-white text-[100px]">Zhi Jun Liang</p>
-          <p className="text-[#BE9D6A] tracking-[7px] text-[24px]">Creative Design</p>
+        <div id="title" className="relative">
+          <img src={img} alt="img" className="w-full hidden md:block"></img>
+          <img src={img1} alt="img" className="w-full block md:hidden"></img>
+          <div className="absolute inset-0 flex flex-col justify-center items-center gap-5 font-arial">
+            <p className="text-white tracking-[7px] md:text-[32px] text-[16px]">Web Development</p>
+            <p className="text-white md:text-[100px] text-[48px]">Zhi Jun Liang</p>
+            <p className="text-[#BE9D6A] tracking-[7px] md:text-[24px] text-[14px]">Creative Design</p>
+          </div>
         </div>
-      </div>
-        
       </>
     );
-  };
+};
   
-  export default Title;
+export default Title;
