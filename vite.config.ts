36c19d7192fe3c3714cb51c13edebf0b13b0b1d1
zhi_jun <liangzhijun1216@gmail.com>
2024-06-01 11:31:46 -0400

    added docker

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-})
+  server: {
+    host: "0.0.0.0", // Listen on all network interfaces
+    port: 5173, // Ensure the port matches the one you're exposing
+    strictPort: true, // Fail if the port is already in use
+  },
+});
